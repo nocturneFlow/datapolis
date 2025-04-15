0a27a07b4faaac845345a8e177ed58533cb9038c
DoZhD1k <chereshnikovarlen03@gmail.com>
2025-04-15 11:36:21 +0500

    feat: revert changes in ui components

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import AppWrapper from "@/components/AppWrapper";
+import AppWrapper from "@/components/AppWrapper"; // <= новый импорт
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -61,7 +61,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
